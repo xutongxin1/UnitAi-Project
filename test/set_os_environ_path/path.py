0dc2774c469a6f76a8e36b0e426ec505e71fd4ea
Jay <50163221+Jay-716@users.noreply.github.com>
2020-02-15 20:56:47 +0800

    123

--- test/set_os_environ_path/path.py
+++ test/set_os_environ_path/path.py
@@ -4,15 +4,13 @@ if(os.path.exists("_conda.exe")):
     path_1 = path + ';'
     path_2 = path + '\\Scripts;'
     path_3 = path + '\\Library\\bin;'
-    path_add = "%path%;" + path_1 + path_2 + path_3
-    command = "set path=" + path_add
+    path_add = "%Path%;" + path_1 + path_2 + path_3
+    command = "set Path=" + path_add
     if os.system(command) == 0:
         os.system("color 2F")
         print("\n\n\n\n\n                  执行成功")
         time.sleep(5)
-        exit()
 else:
     os.system("color 4F")
     print("\n\n\n\n\n           请将此文件放置在Miniconda目录下运行")
     time.sleep(5)
-    exit()
